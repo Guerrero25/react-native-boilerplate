ef8e1e1ad01cca64dbe3b6b50208d5f6c69f3709
Omar Guerrero <guerrero9725@gmail.com>
2019-07-07 10:17:32 -0500

    feat: Added redux-persist

--- App.tsx
+++ App.tsx
@@ -7,17 +7,20 @@ import materialConfig from "./native-base-theme/variables/material";
 
 /* Redux configuration */
 import { Provider as StoreProvider } from "react-redux";
-import store from "./src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import storeConfig from "./src/redux/store";
 
 /* Screens Config */
 import ScreenRoot from "./src/screens";
 
 export default function App() {
   return (
-    <StoreProvider store={store}>
-      <StyleProvider style={getNativeBaseStyle(materialConfig)}>
-        <ScreenRoot />
-      </StyleProvider>
+    <StoreProvider store={storeConfig.store}>
+      <PersistGate loading={null} persistor={storeConfig.persistor}>
+        <StyleProvider style={getNativeBaseStyle(materialConfig)}>
+          <ScreenRoot />
+        </StyleProvider>
+      </PersistGate>
     </StoreProvider>
   );
 }
